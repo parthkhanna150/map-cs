f01e13939bbaea282aae0f0c1cd320b6f40f8bb9
Elisha Lai <elisha.lai@outlook.com>
2017-08-27 09:59:07 -0400

    works

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,6 +4,43 @@ import { Container, Form } from 'semantic-ui-react';
 import Skills from '../constants/Skills';
 
 class SearchForm extends Component {
+
+  handleInput(event, { name, value }) {
+    const n_america = ['JavaScript', 'Python', 'Swift', 'Java'];
+const s_america = ['C', 'JavaScript', 'Python', 'Java'];
+const asia = ['Java', 'C++', 'C', 'Python'];
+const australia = ['Java', 'C', 'Python', 'C++'];
+
+    switch (value) {
+      case 'america':
+        alert(n_america);
+        break;
+
+      case 'canada':
+      alert(n_america);
+      break;
+
+      case 'china':
+      alert(n_america);
+      break;
+
+      case 'japan':
+      alert(asia);
+      break;
+
+      case 'nigeria':
+      alert(australia);
+      break;
+
+      case 'south africa':
+      alert(s_america);
+      break;
+
+      default:
+
+    }
+  }
+
   render() {
     const skillsOptions = Skills;
     const { onAddItem } = this.props;
@@ -15,6 +52,7 @@ class SearchForm extends Component {
       <Form onSubmit={onSubmit}>
         <Form.Group widths={'equal'}>
           <Form.Input
+            onChange={this.handleInput}
             name={'submittedLocation'}
             placeholder={'Location'}
           />
