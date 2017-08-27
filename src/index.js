2044062ffa12cbc54787c00099525fda522db6cf
Elisha Lai <elisha.lai@outlook.com>
2017-08-27 02:33:28 -0400

    add reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import Store from './store/Store';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Provider store={Store}>
+    <App />
+  </Provider>
+  ),
+  document.getElementById('root')
+);
 registerServiceWorker();
